243a1acab7650b3f35546b5283be894447980198
Hurri <thomas.bryer.8@gmail.com>
2020-08-02 18:43:54 +0200

    Clean netlify

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const fs = require('fs')
 const bodyParser = require('body-parser')
-const serverless = require('serverless-http');
 const app = express()
 
 // EJS
